9b1c0761df800533563a23e9c64069907db7968d
Muthoni Kevin Gatamu <49341231+KevGMR@users.noreply.github.com>
2021-11-06 21:43:52 +0300

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -6,7 +6,7 @@ function collect_ratings(){
     }
     let rating = 0;
 
-    const elements = document.querySelectorAll('rating');
+    const elements = document.querySelectorAll('.rating');
 
     elements.forEach((element) => {
         rating = parseInt(element.id.replace("star", ""));
