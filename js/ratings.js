2a909872362db5ffd456b268ab15c8a0af3fcd52
Muthoni Kevin Gatamu <49341231+KevGMR@users.noreply.github.com>
2021-11-06 21:34:22 +0300

    project-add

---
+++ js/ratings.js
@@ -0,0 +1,25 @@
+function collect_ratings(){
+    const ratings = {
+        count : 0,
+        sum : 0,
+        average : 0,
+    }
+    const rating = 0;
+
+    const elements = document.querySelectorAll('rating');
+
+    elements.forEach((element) => {
+        rating = parseInt(element.id.replace("star", ""));
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
+    })
+    if(ratings.count !== 0){
+        ratings.average = ratings.sum/ ratings.count;        
+    }
+    return ratings;
+}
+
+document.addEventListener('change', () => {
+    let ratings = collect_ratings();
+    document.querySelector('#average').value = ratings.average.toFixed(2);
+})
